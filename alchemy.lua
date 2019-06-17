cc810fd22e4cb55ae918bb22f9df09eb67557df9
Izzy <yzziizzy@gmail.com>
2019-06-16 20:46:45 -0600

    hole-digging alchemy

--- alchemy.lua
+++ alchemy.lua
@@ -70,6 +70,29 @@ local function replace_block(min, max, what, with)
 	return true
 end
 
+local function replace_nonempty_block(min, max, with)
+	
+	local node
+	if type(with) == "string" then
+		node = {name = with}
+	else
+		node = with
+	end
+	
+	for x = min.x,max.x,1 do
+	for y = min.y,max.y,1 do
+	for z = min.z,max.z,1 do
+		local n = {x=x, y=y, z=z}
+		if minetest.get_node(n).name ~= "air" then
+			minetest.set_node(n, node)
+		end
+	end
+	end
+	end
+	
+	return true
+end
+
 
 
 local function check_layout(pos, layout)
@@ -141,7 +164,7 @@ end
 
 
 
-local function execute_alchemy(pos, layout)
+local function execute_alchemy(pos, layout, player)
 	local correct = 0
 	
 	for i,v in ipairs(layout) do
@@ -163,6 +186,10 @@ local function execute_alchemy(pos, layout)
 				replace_block(vadd(pos, v.min), vadd(pos, v.max), v.replace, v.with)
 			end
 		end
+		
+		if v.fn then
+			v.fn({x=p.x, y=p.y, z=p.z}, player)
+		end
 	end
 	
 	return true
@@ -177,10 +204,9 @@ potions.alchemy_layouts.sand = {
 	{x=1, z=-1, n="default:sand"},
 	{x=-1, z=1, n="default:sand"},
 	{x=-1, z=-1, n="default:sand",},
-	
 	{y=1, set="default:glass"},
-
 }
+
 potions.alchemy_layouts.dirt = {
 	{x=0, z=0, n="default:stone"},
 	{x=1, z=1, n="default:desert_sand"},
@@ -193,6 +219,194 @@ potions.alchemy_layouts.dirt = {
 	
 	{y=1, set="default:brick"},
 	
+	{particle_spawner = {
+		min={x=-1, z=-1, y=1}, 
+		max={x=1, z=1, y=3},
+		radius = 3,
+		type = "rise",
+		speed = 1,
+		accel = .3,
+		random = .2,
+		density = 100,
+		plife = 2, 
+	}}
+}
+
+
+potions.alchemy_layouts.silver_snake = {
+	{x=0, z=0, n="tnt:tnt"},
+	
+	{x=1, z=1, n="default:mossycobble"},
+	{x=1, z=-1, n="default:mossycobble"},
+	{x=-1, z=1, n="default:mossycobble"},
+	{x=-1, z=-1, n="default:mossycobble",},
+	
+	{x=-2, z=-2, n="default:silver_sandstone_brick",},
+	{x=2, z=-2, n="default:silver_sandstone_brick",},
+	{x=-2, z=2, n="default:silver_sandstone_brick",},
+	{x=2, z=2, n="default:silver_sandstone_brick",},
+	
+	{x=1, z=0, n="default:silver_sandstone_brick",},
+	{x=-1, z=0, n="default:silver_sandstone_brick",},
+	{x=0, z=1, n="default:silver_sandstone_brick",},
+	{x=0, z=-1, n="default:silver_sandstone_brick",},
+	
+	{x=3, z=0, n="default:silver_sandstone_brick",},
+	{x=3, z=-1, n="default:silver_sandstone_brick",},
+	{x=3, z=1, n="default:silver_sandstone_brick",},
+	{x=-3, z=0, n="default:silver_sandstone_brick",},
+	{x=-3, z=-1, n="default:silver_sandstone_brick",},
+	{x=-3, z=1, n="default:silver_sandstone_brick",},
+	{x=0, z=3, n="default:silver_sandstone_brick",},
+	{x=-1, z=3, n="default:silver_sandstone_brick",},
+	{x=1, z=3, n="default:silver_sandstone_brick",},
+	{x=0, z=-3, n="default:silver_sandstone_brick",},
+	{x=-1, z=-3, n="default:silver_sandstone_brick",},
+	{x=1, z=-3, n="default:silver_sandstone_brick",},
+	
+	{fn = function(pos, player)
+-- 		pos.y = pos.y + 1
+-- 		minetest.set_node(pos, {name="default:glass"})
+		
+		local min = vector.add(pos, {x=-3, y=-32, z=-3})
+		local max = vector.add(pos, {x=3, y=-1, z=3})
+		
+		set_block(
+			min, max,
+			"air"
+		)
+		
+		-- flying around
+		minetest.add_particlespawner({
+			amount = 100,
+			time = 5,
+			minpos = {x=min.x, y=max.y+1, z=min.z},
+			maxpos = {x=max.x, y=max.y+1, z=max.z},
+			minvel = {x=-01.01, y=2, z=-01.01},
+			maxvel = {x=01.05,  y=3,  z=01.05},
+			minacc = {x=-01.1, y=0.1, z=-01.1},
+			maxacc = {x=01.1, y=0.3, z=01.1},
+			minexptime = 0.5,
+			maxexptime = 2.5,
+	-- 		collisiondetection = true,
+	-- 		collision_removal = true,
+			minsize = 4,
+			maxsize = 4,
+			texture = "potions_particle.png^[colorize:red:60",
+	-- 		animation = tileanimation
+			glow = 1
+		})
+		
+		-- going down
+		minetest.add_particlespawner({
+			amount = 800,
+			time = 5,
+			minpos = {x=min.x, y=max.y, z=min.z},
+			maxpos = {x=max.x, y=max.y, z=max.z},
+			minvel = {x=-0.01, y=-5, z=-0.01},
+			maxvel = {x=0.05,  y=-6,  z=0.05},
+			minacc = {x=0, y=-0.1, z=0},
+			maxacc = {x=0, y=-1.3, z=0},
+			minexptime = 1.5,
+			maxexptime = 1.5,
+	-- 		collisiondetection = true,
+	-- 		collision_removal = true,
+			minsize = 4,
+			maxsize = 4,
+			texture = "potions_particle.png^[colorize:red:60",
+	-- 		animation = tileanimation
+			glow = 1
+		})
+		
+		
+		minetest.after(1, function() 
+			replace_block(vadd(pos, {x=-3,z=-3}), vadd(pos, {x=3,z=3}), "air", "fire:basic_flame")
+		end)
+		minetest.after(6, function() 
+			set_block(vadd(pos, {x=-3,z=-3}), vadd(pos, {x=3,z=3}), "air")
+			minetest.add_particlespawner({
+				amount = 300,
+				time = 4,
+				minpos = vadd(pos, {x=-3, y=-.5, z=-3}),
+				maxpos = vadd(pos, {x=3, y=-.5, z=3}),
+				minvel = {x=-0.1, y=-.6, z=-0.1},
+				maxvel = {x=0.1,  y=.6,  z=0.1},
+				minacc = {x=-02.1, y=-1.1, z=-02.1},
+				maxacc = {x=02.1, y=1.1, z=02.1},
+				minexptime = 1.5,
+				maxexptime = 1.5,
+				minsize = 2.2,
+				maxsize = 2.2,
+				texture = "potions_particle.png^[colorize:purple:60",
+				glow = 1
+			})
+		end)
+		
+		local function dig(p, s)
+			minetest.set_node({x=p.x, y=p.y-1, z=p.z}, {name="default:silver_sandstone_brick"})
+			
+			if s ~= 0 then
+				minetest.set_node(p, {name="stairs:slab_silver_sandstone_brick"})
+			else
+				minetest.set_node(p, {name="air"})
+			end
+			
+			minetest.set_node({x=p.x, y=p.y+1, z=p.z}, {name="air"})
+			minetest.set_node({x=p.x, y=p.y+2, z=p.z}, {name="air"})
+			minetest.set_node({x=p.x, y=p.y+3, z=p.z}, {name="air"})
+		end
+		
+		
+		local function iterate(i)
+			local m = math.floor(i % 8) 
+			local d = math.floor((i % 32) / 8)
+			local x, z
+			if d < 1 then
+				x = m - 4
+				z = -4
+			elseif d < 2 then
+				x = 4
+				z = m - 4
+			elseif d < 3 then
+				x = 4 - m
+				z = 4
+			else
+				x = -4
+				z = 4 - m
+			end
+				
+			local p = vadd(pos, {x=x, y=math.floor(i/-2), z=z})
+			dig(p, i%2)
+			
+			p.y = p.y - 0.5
+			minetest.add_particlespawner({
+				amount = 80,
+				time = 2,
+				minpos = p,
+				maxpos = p,
+				minvel = {x=-0.1, y=1, z=-0.1},
+				maxvel = {x=0.1,  y=.6,  z=0.1},
+				minacc = {x=-02.1, y=0.1, z=-02.1},
+				maxacc = {x=02.1, y=1.3, z=02.1},
+				minexptime = 1.5,
+				maxexptime = 1.5,
+		-- 		collisiondetection = true,
+		-- 		collision_removal = true,
+				minsize = 2.2,
+				maxsize = 2.2,
+				texture = "potions_particle.png^[colorize:green:60",
+		-- 		animation = tileanimation
+				glow = 1
+			})
+			
+			
+			if i < 64 then
+				minetest.after(1, function() iterate(i+1) end)
+			end
+		end
+		
+		iterate(0)
+	end},
 }
 
 
@@ -220,9 +434,34 @@ minetest.register_craftitem("potions:wand", {
 		
 		local name, def = find_layout(pos)
 		if name then
-			execute_alchemy(pos, def)
+			execute_alchemy(pos, def, user)
 			
 		end
+		
+	end,
+})
+
+
+
+minetest.register_craftitem("potions:book_silver_snake", {
+	description = "Alchemy Wand",
+	inventory_image = "default_book.png^[colorize:white:200",
+	stack_max = 5,
+	on_use = function(itemstack, user, pointed_thing)
+		
+		local pos = pointed_thing.under
+		local def = potions.alchemy_layouts.silver_snake
+		
+		if not pos then
+			return itemstack
+		end
+		
+		if probe_layout(pos, def) then
+			execute_alchemy(pos, def, user)
+			itemstack:take_item()
+		end
+		
+		return itemstack
 		
 	end,
 })
